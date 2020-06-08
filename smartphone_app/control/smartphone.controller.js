484f2b3b60ba3670948179b8cf18a88e575d3eac
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-08 17:31:03 -0300

    Update smartphone.controller.js

--- smartphone_app/control/smartphone.controller.js
+++ smartphone_app/control/smartphone.controller.js
@@ -8,25 +8,25 @@ let urlencodeParser=bodyParser.urlencoded({extended:false});
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-//Adiciona o documento banco de dados
+//Adiciona o documento ao banco de dados
 exports.smartphone_create = ('/create',  function (req,res){
     MongoClient.connect(url, function (err, db) {
-            res.send( "_ID: " + req.body._id + "Nome: "+ req.body.nome + "Marca: " + req.body.marca);
-             var mydocuments = [
-               {
-               _id: req.body._id,
+        res.send( "_ID: " + req.body._id + "Nome: "+ req.body.nome + "Marca: " + req.body.marca);
+        var mydocuments = [
+            {
+                _id: req.body._id,
                nome: req.body.nome,
                marca: req.body.marca}
              ];
+        if (err) throw err;
+        var dbo = db.db("smartphone");
+        dbo.collection("smartphone").insertMany(mydocuments, function  (err, res) {
             if (err) throw err;
-            var dbo = db.db("smartphone");
-            dbo.collection("smartphone").insertMany(mydocuments, function  (err, res) {
-              if (err) throw err;
-              console.log(res.insertedCount+" Documento Inserido ");
-              db.close("smartphone");
-            });
-          });
+            console.log(res.insertedCount+" Documento Inserido ");
+            db.close("smartphone");
+        });
     });
+});
 
 // Atualiza o documento salvo no banco de dados
 exports.smartphone_update = ('/update', function (req,res,mydocuments) {
