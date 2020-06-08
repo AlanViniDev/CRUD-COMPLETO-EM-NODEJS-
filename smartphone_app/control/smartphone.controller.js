d072d2bc06fc0228e22833108712e71848d19155
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-08 17:28:57 -0300

    Update smartphone.controller.js

--- smartphone_app/control/smartphone.controller.js
+++ smartphone_app/control/smartphone.controller.js
@@ -1,5 +1,4 @@
 var Smartphone = require('../model/smartphone.model');
-//require('../route/smartphone.route.js');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
@@ -9,14 +8,7 @@ let urlencodeParser=bodyParser.urlencoded({extended:false});
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-
-exports.testar = function (req, res) {
-    res.send('Olá! Seja Bem vindo !');
-};
-
-//Adiciona ao banco de dados
+//Adiciona o documento banco de dados
 exports.smartphone_create = ('/create',  function (req,res){
     MongoClient.connect(url, function (err, db) {
             res.send( "_ID: " + req.body._id + "Nome: "+ req.body.nome + "Marca: " + req.body.marca);
@@ -36,35 +28,30 @@ exports.smartphone_create = ('/create',  function (req,res){
           });
     });
 
-// Atualiza o banco de dados
+// Atualiza o documento salvo no banco de dados
 exports.smartphone_update = ('/update', function (req,res,mydocuments) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("smartphone");
-
         var _id = req.body._id;
-
         var mydocuments = [
             {$set: {nome: req.body.nome} },
             {$set: {marca: req.body.marca} }
         ]
-        
         dbo.collection("smartphone").updateOne({_id},mydocuments, function  (err, res) {
-            
             if (err) throw err;
             console.log(res.result.nModified +" Documentos Atualizados ");
             db.close("smartphone");
         });
     });
 });
-// deleta o banco de dados
+
+// deleta o documento salvo no banco de dados pelo id
 exports.smartphone_delete = ('/delete', function (req,res) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("smartphone");
-
         var _id = req.body._id;
-
         dbo.collection("smartphone").deleteOne({_id}, function  (err, res) {
             if (err) throw err;
             console.log(" Documento Excluido ");
@@ -72,32 +59,18 @@ exports.smartphone_delete = ('/delete', function (req,res) {
         });
     });
 });
-/*
-exports.smartphone_listar =  function (req,res){
-        const fetch = require('node-fetch');
-        fetch('https://swapi.dev/api/people/1').then((res)=>{
-            return res.json();
-            }).then((json) => {
-                var x = json;
-                //document.getElementById("demo2").innerHTML = (JSON.stringify(x,null,4));
-                res.send("Resultado"+ (JSON.stringify(x,null,4)));
-                 // res.send("Resultado: " + innerHTML(ajax,null,4));
-            });
-};
-*/
 
+// lista todos os documentos salvos no banco de dados
 exports.smartphone_listar = ('/listar',  function (req,res){
     MongoClient.connect(url,  function(err, db) {
         if (err) throw err;
-                var dbo = db.db("smartphone");
-                dbo.collection("smartphone").find({}, {projection: {} }).toArray(function(err, result) {
-                  if (err) throw err;
-                  console.log(result);
-                  res.send(result);
-                 //res.send("Resultado" + (JSON.stringify(result,null,4)));
-                 // res.send("Resultado: " + innerHTML(ajax,null,4));
-                });
+        var dbo = db.db("smartphone");
+        dbo.collection("smartphone").find({}, {projection: {} }).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
         });
+    });
 });
 
 
