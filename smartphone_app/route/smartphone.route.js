67229ea412530d014e7781bc3392c8899244fc88
Alan Vinicius <alan123vinicius@gmail.com>
2020-06-08 17:23:25 -0300

    Update smartphone.route.js

--- smartphone_app/route/smartphone.route.js
+++ smartphone_app/route/smartphone.route.js
@@ -1,3 +1,4 @@
+// requisições
 const express = require('express');
 const bodyParser = require("body-parser");
 const router = express.Router();
@@ -9,12 +10,12 @@ router.use(bodyParser.json());
 const smartphone_controller = require("../control/smartphone.controller.js");
 const view_controller =  require("../control/view.controller.js");
 
-
+//rotas
 router.get('/testar', smartphone_controller.testar); 
 router.post('/create', smartphone_controller.smartphone_create);
 router.post('/update', smartphone_controller.smartphone_update);
 router.post('/delete',smartphone_controller.smartphone_delete);
 router.get('/listar',smartphone_controller.smartphone_listar);
 router.get('/index', view_controller.view);
-//router.get('/:id', smartphone_controller.details);
+
 module.exports = router;  
